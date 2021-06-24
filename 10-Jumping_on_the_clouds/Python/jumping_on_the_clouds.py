d51343d1d82ca62298db2274ee7848fdf595f970
Richard Firdaus <richfir@gmail.com>
2021-06-24 21:15:59 +0800

    10 - Jumping on the clouds

---
+++ 10-Jumping_on_the_clouds/Python/jumping_on_the_clouds.py
@@ -0,0 +1,80 @@
+'''
+https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. It is always possible to win the game.
+
+For each game, you will get an array of clouds numbered  if they are safe or  if they must be avoided.
+
+Example
+
+Index the array from . The number on each cloud is its index in the list so the player must avoid the clouds at indices  and . They could follow these two paths:  or . The first path takes  jumps while the second takes . Return .
+
+Function Description
+
+Complete the jumpingOnClouds function in the editor below.
+
+jumpingOnClouds has the following parameter(s):
+
+int c[n]: an array of binary integers
+Returns
+
+int: the minimum number of jumps required
+Input Format
+
+The first line contains an integer , the total number of clouds. The second line contains  space-separated binary integers describing clouds  where .
+
+Constraints
+
+Output Format
+
+Print the minimum number of jumps needed to win the game.
+
+Sample Input
+7
+0 0 1 0 0 1 0
+
+Sample Output
+4
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    count = 0
+    i = 0
+    while i <= (len(c)-2):
+        if i < (len(c)-2) and c[i+2] == 0:
+            i += 2
+            count += 1
+                
+        elif c[i+1] == 0:
+            i += 1
+            count += 1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
